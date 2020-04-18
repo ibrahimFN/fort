f6ef62a70ee9d7cf91fd09f9bdb471f49f58642c
gomashio1596 <53356872+gomashio1596@users.noreply.github.com>
2020-04-18 17:07:21 +0900

    バックアップ時のエラーを修正

--- auto-updater.py
+++ auto-updater.py
@@ -1,4 +1,9 @@
-import time,requests,sys,os,json,traceback
+import traceback
+import requests
+import time
+import json
+import sys
+import os
 
 def AddNewKey(data: dict, new: dict) -> dict:
     result = data.copy()
@@ -10,92 +15,110 @@ def AddNewKey(data: dict, new: dict) -> dict:
 
 def CheckUpdate(filename: str, githuburl: str) -> bool:
     print(f'{filename} のアップデートを確認中...')
-    for count, text in enumerate(filename[::-1]):
-        if text == ".":
-            filename_ = filename[:len(filename)-count-1]
-            extension = filename[-count-1:]
-            break
-    else:
-        extension == ""
-    if extension == ".py" or extension == "":
-        if os.path.isfile(filename):
-            with open(filename, encoding='utf-8') as f:
-                current = f.read()
+    try:
+        for count, text in enumerate(filename[::-1]):
+            if text == ".":
+                filename_ = filename[:len(filename)-count-1]
+                extension = filename[-count-1:]
+                break
         else:
-            github = requests.get(githuburl + filename)
-            github.encoding = github.apparent_encoding
-            github = github.text.encode(encoding='utf-8')
-            with open(filename, 'bw') as f:
-                f.write(github)
-            with open(filename, encoding='utf-8') as f:
-                current = f.read()
-        github = requests.get(githuburl + filename)
-        github.encoding = github.apparent_encoding
-        github = github.text.encode(encoding='utf-8')
-        if current.replace('\n','').replace('\r','').encode(encoding='utf-8') != github.decode().replace('\n','').replace('\r','').encode(encoding='utf-8'):
-            print(f'{filename} のアップデートを確認しました!')
-            print(f'{filename} をバックアップ中...')
-            try:
-                os.rename(filename, f'{filename_}_old{extension}')
-            except PermissionError:
-                print(f'{filename} ファイルをバックアップできませんでした。\n{traceback.format_exc()}')
-                exit()
+            extension == ""
+        if extension == ".py" or extension == ".bat" or extension == ".txt" or extension == "":
+            if os.path.isfile(filename):
+                with open(filename, encoding='utf-8') as f:
+                    current = f.read()
             else:
+                github = requests.get(githuburl + filename)
+                github.encoding = github.apparent_encoding
+                github = github.text.encode(encoding='utf-8')
                 with open(filename, 'bw') as f:
                     f.write(github)
-                print(f'{filename} の更新が完了しました!\n')
-                CheckUpdate("config.json", githuburl)
-                CheckUpdate("commands.json", githuburl)
-                return True
-        else:
-            print(f'{filename} の更新はありません!\n')
-            return False
-    elif extension == ".json":
-        if os.path.isfile(filename):
-            with open(filename, encoding='utf-8') as f:
-                current = f.read()
-        else:
+                with open(filename, encoding='utf-8') as f:
+                    current = f.read()
             github = requests.get(githuburl + filename)
             github.encoding = github.apparent_encoding
             github = github.text.encode(encoding='utf-8')
-            with open(filename, 'bw') as f:
-                f.write(github)
-            with open(filename, encoding='utf-8') as f:
-                current = f.read()
-        current = json.loads(current)
-        github = requests.get(githuburl + filename)
-        github.encoding = github.apparent_encoding
-        github = github.text
-        
-        github = json.loads(github)
-        new = AddNewKey(current, github)
-        if current != new:
-            print(f'{filename} のアップデートを確認しました!')
-            print(f'{filename} をバックアップ中...')
-            try:
+            if current.replace('\n','').replace('\r','').encode(encoding='utf-8') != github.decode().replace('\n','').replace('\r','').encode(encoding='utf-8'):
+                print(f'{filename} のアップデートを確認しました!')
+                print(f'{filename} をバックアップ中...')
                 if os.path.isfile(f'{filename_}_old{extension}'):
                     try:
                         os.remove(f'{filename_}_old{extension}')
                     except PermissionError:
-                        print(f'{filename_}_old{extension} ファイルを削除できませんでした。\n{traceback.format_exc()}')
-                os.rename(filename, f'{filename_}_old{extension}')
-            except PermissionError:
-                print(f'{filename} ファイルをバックアップできませんでした。\n{traceback.format_exc()}')
-                exit()
+                        print(f'{filename} ファイルを削除できませんでした。\n{traceback.format_exc()}')
+                try:
+                    os.rename(filename, f'{filename_}_old{extension}')
+                except PermissionError:
+                    print(f'{filename} ファイルをバックアップできませんでした。\n{traceback.format_exc()}')
+                else:
+                    with open(filename, 'bw') as f:
+                        f.write(github)
+                    print(f'{filename} の更新が完了しました!\n')
+                    CheckUpdate("auto-updater.py", githuburl)
+                    CheckUpdate("requirements.txt", githuburl)
+                    CheckUpdate("config.json", githuburl)
+                    CheckUpdate("commands.json", githuburl)
+                    CheckUpdate("Check update.bat", githuburl)
+                    CheckUpdate("INSTALL IFNOTWORK.bat", githuburl)
+                    return True
             else:
-                with open(filename, 'w', encoding="utf-8") as f:
-                    json.dump(new, f, indent=4, ensure_ascii=False)
-                print(f'{filename} の更新が完了しました!\n')
-                return True
+                print(f'{filename} の更新はありません!\n')
+                return False
+        elif extension == ".json":
+            if os.path.isfile(filename):
+                with open(filename, encoding='utf-8') as f:
+                    current = f.read()
+            else:
+                github = requests.get(githuburl + filename)
+                github.encoding = github.apparent_encoding
+                github = github.text.encode(encoding='utf-8')
+                with open(filename, 'bw') as f:
+                    f.write(github)
+                with open(filename, encoding='utf-8') as f:
+                    current = f.read()
+            current = json.loads(current)
+            github = requests.get(githuburl + filename)
+            github.encoding = github.apparent_encoding
+            github = github.text
+            
+            github = json.loads(github)
+            new = AddNewKey(current, github)
+            if current != new:
+                print(f'{filename} のアップデートを確認しました!')
+                print(f'{filename} をバックアップ中...')
+                try:
+                    if os.path.isfile(f'{filename_}_old{extension}'):
+                        try:
+                            os.remove(f'{filename_}_old{extension}')
+                        except PermissionError:
+                            print(f'{filename_}_old{extension} ファイルを削除できませんでした。\n{traceback.format_exc()}')
+                    os.rename(filename, f'{filename_}_old{extension}')
+                except PermissionError:
+                    print(f'{filename} ファイルをバックアップできませんでした。\n{traceback.format_exc()}')
+                    exit()
+                else:
+                    with open(filename, 'w', encoding="utf-8") as f:
+                        json.dump(new, f, indent=4, ensure_ascii=False)
+                    print(f'{filename} の更新が完了しました!\n')
+                    return True
+            else:
+                print(f'{filename} の更新はありません!\n')
+                return False
         else:
-            print(f'{filename} の更新はありません!\n')
-            return False
-    else:
-        print(f'拡張子 {extension} は対応していません\n')
-        exit()
+            print(f'拡張子 {extension} は対応していません\n')
+    except Exception:
+        print("アップデートに失敗しました")
+        print(traceback.format_exc())
 
-githuburl = "https://raw.githubusercontent.com/gomashio1596/Fortnite-LobbyBot/master/"
+if "-dev" in sys.argv:
+    githuburl = "https://raw.githubusercontent.com/gomashio1596/Fortnite-LobbyBot/Dev/"
+else:
+    githuburl = "https://raw.githubusercontent.com/gomashio1596/Fortnite-LobbyBot/master/"
 if CheckUpdate("index.py", githuburl) is False and "-all" in sys.argv:
+    CheckUpdate("auto-updater.py", githuburl)
+    CheckUpdate("requirements.txt", githuburl)
     CheckUpdate("config.json", githuburl)
     CheckUpdate("commands.json", githuburl)
+    CheckUpdate("Check update.bat", githuburl)
+    CheckUpdate("INSTALL IFNOTWORK.bat", githuburl)
 print("すべてのアップデートが完了しました")
