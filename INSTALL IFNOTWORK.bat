893b333b5d377d7294663e2656c2478deb549305
gomashio1596 <53356872+gomashio1596@users.noreply.github.com>
2020-06-08 15:50:15 +0900

    Update INSTALL IFNOTWORK.bat

--- INSTALL IFNOTWORK.bat
+++ INSTALL IFNOTWORK.bat
@@ -3,4 +3,5 @@ py -3 -m pip install -U discord.py
 py -3 -m pip install -U requests
 py -3 -m pip install -U crayons
 py -3 -m pip install -U jaconv
-py -3 -m pip install -U flask
+py -3 -m pip install -U jinja2
+py -3 -m pip install -U sanic
